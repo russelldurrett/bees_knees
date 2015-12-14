a4c2cec9d5dd694b148a062889800563fc52ea1f
Drew Vander Wood <dvanderwood@gmail.com>
2015-12-14 15:44:28 -0600

    Comments

--- fastq_shuffler.py
+++ fastq_shuffler.py
@@ -5,6 +5,7 @@ usage: python fastq_shuffler.py sample_list.txt
 '''
 
 #Sample Text should be unique identifiers for each data set. So App2_1_GCCAATAT_L001_R1_001.fastq.gz and App2_1_GCCAATAT_L001_R2_001.fastq.gz would be represented as App2_1
+#Should be working in the folder where the fastq.gz's (your sequence data) are located
 
 old_names = open(sys.argv[1]).readlines()
 names = []
@@ -14,7 +15,8 @@ for name in old_names:
 print 'working with these samples:'
 print names 
 
-#fastq_list = [f for f in os.listdir('.') if os.path.isfile(f)]
+
+#.fastq is working with fastqs (or whatever the format the sequence data is in), .gz if working with zipped files
 fastq_list = glob.glob('*.gz')
 print 'All fastq files:'
 print fastq_list
@@ -25,6 +27,7 @@ for sample in names:
 	sample_fastqs = [filename for filename in fastq_list if sample_name in filename]
 #	sample_fastqs = [f for f in sample_fastqs]
 	print 'found these fastqs: ' + ' '.join(sample_fastqs)
+	#remove .gz if working with unzipped fastqs (or whatever file format), change pathway to get to your velvet directory and the directory where you want the interleaved sequences saved
 	args_str = '../tools/velvet_1.2.10/contrib/shuffleSequences_fasta/shuffleSequences_fastq.pl ' + ' '.join(sample_fastqs) + ' ../genomes_batch1_merged/' + str(sample_name) + '_merged.fastq.gz'
 	print args_str
 	args = str.split(args_str)
--- velveth_run.py
+++ velveth_run.py
@@ -4,6 +4,9 @@ import sys, os, glob, subprocess
 usage: velveth_run.py sample_list.txt
 '''
 #Sample Text should be unique identifiers for each data set. So App2_1_GCCAATAT_L001_R1_001.fastq.gz and App2_1_GCCAATAT_L001_R2_001.fastq.gz would be represented as App2_1
+#and each identifier should be on its own line
+#Should be working in the directory containg the sequences you want to run, for example if you have interleaved data in the directory /genomes_batch1_merged you should run this
+#in that directory
 
 old_names = open(sys.argv[1]).readlines()
 names = []
@@ -13,6 +16,7 @@ for name in old_names:
 print 'working with these samples:'
 print names 
 
+#.fastq is working with fastqs (or whatever the format the sequence data is in), .gz if working with zipped files
 fastq_list = glob.glob('*.gz')
 print 'All fastq files:'
 print fastq_list
@@ -23,6 +27,13 @@ for sample in names:
 	sample_fastqs = [filename for filename in fastq_list if sample_name in filename]
 #	sample_fastqs = [f for f in sample_fastqs]
 	print 'found these fastqs: ' + ' '.join(sample_fastqs)
+	# Change pathway to get to your velvet directory
+	# THe second part of the string sequence names the directory with the name from the sample_list.txt
+	# The number in the third part of the string is the kmer length and can be changed (if testing multiple kmer lengths it would be worth it to include a identifier on the directory name)
+	# This could be done by adding + '_95' after the first str(sample+name)
+	# The second identifier in the third part of the string sequence is the file type and should be changed appropiately
+	# The third identifier in the third part of the string sequence is the read type and should be chnaged appropiately
+	# Remove .gz if working with unzipped fastqs (or whatever file format) from the part of the string sequence
 	args_str = '../tools/velvet_1.2.10/velveth ' + str(sample_name) + ' 95 -fastq.gz -shortPaired ' + str(sample_name) + '_merged.fastq.gz'
 	print args_str
 	args = str.split(args_str)
